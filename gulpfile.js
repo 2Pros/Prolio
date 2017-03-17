def2bd7bc64542353815d50f0f84d70a1d217444
Bazzigar <anumunnny@gmail.com>
2017-03-18 02:46:29 +0530

    re-structured directory to follow best practices

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,23 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  return gulp.src('app/sass/**/*.scss')
+  return gulp.src('app/assets/sass/**/*.scss')
   .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(gulp.dest('app/css'))
+  .pipe(gulp.dest('app/assets/css'))
   .pipe(browserSync.reload({
       stream: true
     }))
 });
 
 gulp.task('watch', ['browserSync'], function() {
-    gulp.watch('app/sass/**/*.scss', ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch('assets/sass/**/*.scss', ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/js/*.js')
+  return gulp.src('app/*.js')
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('app/dist/'));
+    .pipe(gulp.dest('./src/'));
 });
 
 gulp.task('browserSync', function() {
